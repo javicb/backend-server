542e41a678c36d8383460e9c5290daf03bd43f53
Javier Cruz <javi.cb@gmail.com>
2021-04-25 17:48:23 +0200

    get usuarios

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,9 +1,12 @@
 const Usuario = require('../models/usuario');
 
-const getUsuarios = (request, response) => {
+const getUsuarios = async (request, response) => {
+
+  const usuarios = await Usuario.find({}, 'nombre email role');
+
   response.json({
     ok: true,
-    msg: 'getUsuarios'
+    usuarios
   })
 }
 
