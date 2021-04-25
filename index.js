d327d54742f7d92f4bbf28b09d9af854dde6d0e5
Javier Cruz <javi.cb@gmail.com>
2021-04-25 11:40:55 +0200

    primera ruta

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const express = require('express');
 // Crear servidor express
 const app = express();
 
+// RUgas
+app.get('/', (request, response) => {
+  response.json({
+    ok: true,
+    msg: 'Hola mundo'
+  })
+});
+
 app.listen(3000, () => {
   console.log('servidor levantado en puertooo: 3000');
 });
